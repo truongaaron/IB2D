90292788660b569415330db87e04023ddc8df921
truongaaron <plainaaron@gmail.com>
2019-10-23 09:55:21 -0700

    movement fixed to be more slippery

--- IceBaneling2D/EnemyCircle.gd
+++ IceBaneling2D/EnemyCircle.gd
@@ -3,22 +3,24 @@ extends KinematicBody2D
 #export var speed = 3
 export var width = 200
 export var height = 200
-
+export var frequency = 0.5
+var initial_x = 0
+var initial_y = 0
 var timeCounter = 0
 
+func _ready():
+	initial_x = position.x
+	initial_y = position.y
+
 func _process(delta):
-	print(delta)
+	
 	timeCounter += (delta*3) # Speed is hardcoded here
 							 # I Don't know why the global speed variable doesn't work
 	
-	var x = cos(timeCounter)
-	var y = sin(timeCounter)
+	var x =cos(frequency*timeCounter)
+	var y = sin(frequency*timeCounter)
 	
-	print(x)
-	print(" ")
-	print(y)
-	
-	position.x = (x * width)
-	position.y = (y * height)
+	position.x = initial_x+ (x * width)
+	position.y = initial_y+(y * height)
 	
 	
--- IceBaneling2D/Player.gd
+++ IceBaneling2D/Player.gd
@@ -6,16 +6,17 @@ var acceleration = Vector2(0, 0)
 var maxspeed = 4
 var maxforce = .1
 
+export var default_friction = 0.5
+export var ice_friction = 0.01
 
 var destination = Vector2()
 var on_ice_destination = Vector2()
 
 var gap = Vector2()
-var speed = null
+export var speed = 200
 var on_ice = false
 
 func _ready():
-	speed = 100
 	destination = Vector2(position.x, position.y)
 
 func _input(event):
@@ -25,28 +26,46 @@ func _input(event):
 		update()
 
 func _process(delta):
-	if on_ice and position != on_ice_destination:
-		gap = Vector2(on_ice_destination.x - position.x, on_ice_destination.y - position.y)
-		move_and_slide(gap.normalized() * speed)
-		if gap.abs() < Vector2(1,1):
-			set_position(on_ice_destination)
-	if on_ice_destination == position:
-		move_and_slide(gap*0)
+	var old_way = false
+	if old_way:
+		if on_ice and position != on_ice_destination:
+			gap = Vector2(on_ice_destination.x - position.x, on_ice_destination.y - position.y)
+			move_and_slide(gap.normalized() * speed)
+			if gap.abs() < Vector2(1,1):
+				set_position(on_ice_destination)
+		if on_ice_destination == position:
+			move_and_slide(gap*0)
+			
+		if position != destination:
+			gap = Vector2(destination.x - position.x, destination.y - position.y)
+			move_and_slide(gap.normalized() * speed)
+			if gap.abs() < Vector2(1,1):
+				set_position(destination)
+		if destination == position:
+			move_and_slide(gap*0)
+	else:
 		
-	if position != destination:
 		gap = Vector2(destination.x - position.x, destination.y - position.y)
-		move_and_slide(gap.normalized() * speed)
-		if gap.abs() < Vector2(1,1):
-			set_position(destination)
-	if destination == position:
-		move_and_slide(gap*0)
-
+		var direction = gap.normalized()
+		if gap.length() < 1:
+			direction = gap
+		var desired_velocity = direction*speed
+		var friction = default_friction
+		if on_ice:
+			friction = ice_friction
+		
+		velocity = friction*desired_velocity + (1-friction)*velocity
+		
+		move_and_slide(velocity)
+		
+			
 func update():
-	velocity += acceleration
-	limit(maxspeed, velocity)
-	location += velocity
-	acceleration *= 0
-
+	#velocity += acceleration
+	#limit(maxspeed, velocity)
+	#location += velocity
+	#acceleration *= 0
+	pass
+	
 func applyForce(force):
 	acceleration += force
 
